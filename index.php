e82b0adc7615143c4031582d0e3bf538bbf098ce
isurutmv <isurutmv@gmail.com>
2019-06-29 10:08:22 +0530

    add chat box model

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ if (!isset($_SESSION['user_id']))
         <p align="right">Hi  <?php echo $_SESSION['username'];?> <a href="logout.php"> Logout</a></p>
 
         <div id="user_details"></div>
+        <div id="user_model_details"></div>
     </div>
 
 </div>
@@ -76,5 +77,35 @@ if (!isset($_SESSION['user_id']))
                 }
             })
         }
+
+        function make_chat_dialog_box(to_user_id,to_user_name) {
+
+             var model_content = '<div id="user_dialog_'+to_user_id+'" class="user_dialog" title="You Have Chat With '+to_user_name+'">';
+
+             model_content +='<div style="height:400px; border: 1px solid #ccc; overflow-y: scroll; margin-bottom: 24px; padding: 16px;" class="chat_history" data-touserid="'+to_user_id+'" data-tousername="'+to_user_name+'">'
+
+            model_content += '</div>';
+             model_content+= '<div class="form-group">';
+            model_content += '<textarea name="chat_message_'+to_user_id+'" id="chat_message_'+to_user_id+'" class="form-control"></textarea>';
+            model_content += '</div><div class="form-group" align="right">';
+            model_content+= '<button type="button" name="send_chat" id="'+to_user_id+'" class="btn btn-info send_chat">Send</button></div></div>';
+
+            $('#user_model_details').html(model_content);
+        }
+
+        $(document).on('click','.start_chat',function () {
+
+            var to_user_id = $(this).data('touserid');
+            var to_user_name = $(this).data('tousername');
+
+            make_chat_dialog_box(to_user_id,to_user_name);
+
+            $("#user_dialog_"+to_user_id).dialog({
+                autoOpen:false,
+                width:400
+            });
+
+            $("#user_dialog_"+to_user_id).dialog('open');
+        });
     });
 </script>
